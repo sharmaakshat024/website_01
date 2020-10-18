1d2169e37ee4541a5b49d4a7e447b1301d146dc5
Akshat <sharmaakshat208@gmail.com>
2020-10-18 11:57:17 +0530

    https://github.com/sharmaakshat024/simple_website

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from home.models import Contact
 
 # Create your views here.
 def home(request):
@@ -15,5 +16,15 @@ def projects(request):
    return render(request, 'projects.html')
 
 def contact(request):
+   if request.method=="POST":
+     
+      name = request.POST['name']
+      email = request.POST['email']
+      phone = request.POST['phone']
+      desc = request.POST['desc']
+     # print(name, email, phone, desc)
+      ins = Contact(name=name, email=email, phone=phone, desc=desc)
+      ins.save()
+      print("The data has been written to the db")
    # return HttpResponse("This is my contact page(/contact)")
    return render(request, 'contact.html')
