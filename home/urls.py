9be2704b5ed02d1d61c53ad5eef6ca4067c99241
Akshat <sharmaakshat208@gmail.com>
2020-10-24 11:24:23 +0530

    commit_first

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from home import views
 
+
+# Django admin header customization
+
+admin.site.site_header = "Login to Developer AKSHAT"
+admin.site.site_title = "Welcome to Akshat's Dashboard"
+admin.site.index_title = "Welcome to this Portal"
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
--- home/views.py
+++ home/views.py
@@ -23,8 +23,8 @@ def contact(request):
       phone = request.POST['phone']
       desc = request.POST['desc']
      # print(name, email, phone, desc)
-      ins = Contact(name=name, email=email, phone=phone, desc=desc)
-      ins.save()
+      contact = Contact(name=name, email=email, phone=phone, desc=desc)
+      contact.save()
       print("The data has been written to the db")
    # return HttpResponse("This is my contact page(/contact)")
    return render(request, 'contact.html')
